918498e7123cb5c83ec66c751be0ff84682c023f
Yun William Yu <ywy@mit.edu>
2016-05-15 17:33:06 -0400

    Playing with variables

--- tensorflow_tutorial_singlelayer.py
+++ tensorflow_tutorial_singlelayer.py
@@ -18,6 +18,9 @@ def conv2d(x, W):
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
+def max_pool_4x4(x):
+    return tf.nn.max_pool(x, ksize=[1,4,4,1], strides=[1,4,4,1], padding='SAME')
+
 x = tf.placeholder(tf.float32, [None,784])
 
 y_ = tf.placeholder(tf.float32, [None,10])
@@ -26,19 +29,22 @@ y_ = tf.placeholder(tf.float32, [None,10])
 
 #train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
+# 5x5 input patch, 1 input b/w, 32 features out
 W_conv1 = weight_variable([5,5,1,32])
 b_conv1 = bias_variable([32])
 
+# 4D tensor, 1st argument "-1 = None", 28x28 = 784 pixels, 1 b/w channel
 x_image = tf.reshape(x, [-1,28,28,1])
 
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+#h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+#h_pool1 = max_pool_2x2(h_conv1)
 
-W_conv2 = weight_variable([5,5,32,64])
+W_conv2 = weight_variable([5,5,1,64])
 b_conv2 = bias_variable([64])
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+h_conv2 = tf.nn.relu(conv2d(x_image, W_conv2) + b_conv2)
+#h_pool2 = max_pool_2x2(h_conv2)
+h_pool2 = max_pool_4x4(h_conv2)
 
 W_fc1 = weight_variable([7*7*64, 1024])
 b_fc1 = bias_variable([1024])
@@ -66,7 +72,7 @@ sess.run(init)
 
 for i in range(1000):
     batch = mnist.train.next_batch(50)
-    if i%10 == 0:
+    if i%100 == 0:
         train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_:batch[1], keep_prob:1.0})
         print("step %d, training accuracy %g"%(i, train_accuracy))
     train_step.run(feed_dict={x:batch[0], y_:batch[1], keep_prob:0.5})
